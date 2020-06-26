d6495db1f9f7fef3ba3da090a01e6c0dedbda460
lauriely lourenço <laurielylourenco@gmail.com>
2020-06-26 16:14:09 -0300

    creating css and html

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var cidade = document.getElementById('cidade');
+const chave = "1e88f70422cf39e04785fea7cec7c376";
+//var local = window.localStorage;
+var cidade = cidade.value;
+console.log(cidade);
+function buscarPrevisao() {
+    
+ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chave}`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+		"x-rapidapi-key": `${chave}`
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.log(err);
+});
+  
+}
+
+
+//let  api =  api.openweathermap.org/data/2.5/weather?q={city}&appid={chave}
+ 
+
+/*let cam =  api + cidade +"&appid="+chave
+    console.log(cidade)
+    console.log(cam)
+    let dados = cam;
+    let obj = JSON.stringify({dados});
+    console.log(obj)
+*/
