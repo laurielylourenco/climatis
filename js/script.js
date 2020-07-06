2ebc0f59f615f27980580fbe77cf414f37aa5c06
laurielylourenco <laurielylourenco@gmail.com>
2020-07-06 18:55:44 -0300

    update

--- js/script.js
+++ js/script.js
@@ -3,27 +3,28 @@ const chave = "1e88f70422cf39e04785fea7cec7c376";
 //var local = window.localStorage;
 var cidade = cidade.value;
 console.log(cidade);
+
 function buscarPrevisao() {
-    
- fetch("https://api.openweathermap.org/data/2.5/weather?q="+cidade+"&appid="+chave+"&lang=pt-br", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-		"x-rapidapi-key": `${chave}`
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.log(err);
-});
-  
+
+	fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=" + chave + "&lang=pt-br", {
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+				"x-rapidapi-key": `${chave}`
+			}
+		})
+		.then(response => {
+			console.log(response);
+		})
+		.catch(err => {
+			console.log(err);
+		});
+
 }
 
 
 //let  api =  api.openweathermap.org/data/2.5/weather?q={city}&appid={chave}
- 
+
 
 /*let cam =  api + cidade +"&appid="+chave
     console.log(cidade)
