c536a77228034cb3da2728c60ff58c593d2c9c72
laurielylourenco <laurielylourenco@gmail.com>
2020-07-14 20:56:16 -0300

    update fetch API

--- js/script.js
+++ js/script.js
@@ -1,37 +1,33 @@
-var cidade = document.getElementById('city');
+var cidade = document.getElementById('cidade');
 const chave = "1e88f70422cf39e04785fea7cec7c376";
 var res = document.getElementById('resultado')
-//var local = window.localStorage;
-
 
 function buscarPrevisao() {
     var c = cidade.value;
     var linguagem = "pt-br";
     var unidade = "metric";
-    const myHeaders = new Headers({
-        "Content-Type": "text/plain",
-        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-        "x-rapidapi-key": `${chave}`
-
-    });
-
     var url = "https://api.openweathermap.org/data/2.5/weather?q=" + c + "&appid=" + chave + "&lang=" + linguagem + "&units=" + unidade + "";
-    var req = new Request(url, {
-        "method": "GET",
-        "headers": myHeaders,
-        "mode": "no-cors",
-        "credentials": "include",
-    });
 
-    fetch(req)
+
+    fetch(url, {
+            method: "GET",
+        })
         .then(response => {
-       console.log(response);
-            
+            if (response.ok) {
+                console.log(response.url)
+                return response.json()
+            } else {
+                return Promise.reject({
+                    status: response.status,
+                    statusText: response.statusText
+                });
+            }
+
+        })
+        .then(function (data) {
+            console.log(data)
         })
         .catch(error => {
             console.log(error);
-            
-
         });
-
 }
