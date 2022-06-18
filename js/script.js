62a996510b2a61ba8f3a68372d8ba321f83437d4
lauriely lourenço <laurielylourenco@gmail.com>
2022-06-18 15:34:35 -0300

    Update: layout

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 var cidade = document.getElementById('cidade');
 const chave = "1e88f70422cf39e04785fea7cec7c376";
 var res = document.getElementById('resultado')
-//
+
 if (chave == '') {
-    document.getElementById('result').innerHTML = "<h1>Precisa de uma chave!!!</h1> <br> Encontre no site da api openweathermap";
+    document.getElementById('des').innerHTML = "<h1>Precisa de uma chave!!!</h1> <br> Encontre no site da api openweathermap";
 }
 function buscarPrevisao() {
     var c = cidade.value;
     var linguagem = "pt_br";
     var url = "https://api.openweathermap.org/data/2.5/weather?q="+c+"&units=metric"+"&appid="+chave+"&lang="+linguagem+"";
+    var clean =   document.getElementById('cidade');
+    clean.value = "";
 
+    
     fetch(url, {
             method: "GET",
         })
@@ -37,19 +40,8 @@ function mostrarPrevisao(dados) {
 
    var des = dados.weather[0].description;
    
- 
-   if(des == "nublado" || des == "nuvens dispersas"){
-        document.querySelector("#corpo").style.background = "linear-gradient(to right, #8f94fb, #4e54c8)";
-    } else if (des == "chuva leve"){
-
-        document.querySelector("#corpo").style.background = "linear-gradient(to right, #fff, #076585)";
-    }
-    else{
-
-        document.querySelector("#corpo").style.background = "linear-gradient(to right, #F37335, #FDC830)";
-    } 
     document.getElementById('des').innerHTML = dados.weather[0].description;
-    document.getElementById('min').innerHTML = dados.main.temp_min+"<code>&deg;</code>";
+    document.getElementById('min').innerHTML = dados.main.temp_min+"<code>&deg;</code>"+"c";
     document.getElementById('local').innerHTML = dados.name;
     
 }
