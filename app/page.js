58467fccfb0007f00754b996c74e79c339d7b0d2
lauriely lourenço <49760702+laurielylourenco@users.noreply.github.com>
2023-06-01 00:04:50 -0300

    layout inicial

--- app/page.js
+++ app/page.js
@@ -1,3 +1,38 @@
-export default function Page() {
-  return <h1>aprendendo na pratica!</h1>;
+'use client';
+import { useState } from 'react';
+import './componets/Formulario.css';
+/* import { useState } from 'react'; */
+
+
+
+export default async function Page() {
+
+  const [cidade, setCidade] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aqui você pode fazer algo com os dados do formulário, como enviar para um banco de dados ou fazer uma requisição HTTP.
+
+    console.log('cidade:', cidade);
+
+  };
+
+  return (
+
+    <div className="corpo container">
+
+      <div className="panel box">
+
+        <form >
+
+          <input type="text" placeholder="Digite nome da cidade" />
+          <button>Buscar</button>
+        </form>
+
+        <div className='respostaTempo'>
+
+        </div>
+      </div>
+    </div>
+  )
 }
